b2acfb78d4a7306d425ff54704f33c2796d28132
irwin <ijesperanza@innovacion.gob.sv>
2024-10-22 08:21:00 -0600

    cambios para swagger en plantilla

---
+++ app/Swagger/SwaggerAnnotations.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Swagger;
+
+/**
+ * @OA\Info(
+ *     title="My API",
+ *     version="1.0.0",
+ *     description="API documentation for My Application",
+ *     @OA\Contact(
+ *         email="support@example.com"
+ *     ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * )
+ */
+class SwaggerAnnotations
+{
+    // This class is used solely to hold the @OA\Info annotation
+}
--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,9 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 Route::prefix('auth')->group(function (){
     Route::post('login', [AuthController::class,'login']);
